890eab375dfc281a2f11af9101cc77b7bcd31ef5
ryutaro <m2002r1028@icloud.com>
2023-12-21 17:03:00 +0900

    add: グラフ作成

---
+++ draw.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+
+def plot_csv_data(file_path):
+    # CSVファイルからデータを読み込む
+    df = pd.read_csv(file_path, header=None, names=["x", "y", "label"])
+
+    print(df)
+
+    # データをプロット
+    plt.scatter(
+        df['x'], df['y'], c=df['label'], cmap='viridis', marker='o')
+
+    # グラフの設定
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    # グラフを表示
+    plt.show()
+
+
+if __name__ == "__main__":
+    # コマンドライン引数からファイルパスを取得
+    file_path = sys.argv[1]
+
+    # CSVファイルからデータを読み込み、プロットする
+    plot_csv_data(file_path)
