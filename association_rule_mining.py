cd450c9734ea6945e258162315bc3a3a652718ff
ryutaro <m2002r1028@icloud.com>
2024-01-14 18:39:21 +0900

    fix: クラス化した

--- association_rule_mining.py
+++ association_rule_mining.py
@@ -4,120 +4,130 @@ import sys
 import pandas as pd
 
 
-def encode_onehot(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    one-hotエンコーディングを行う
+class OneHotEncoder:
+    def __init__(self) -> None:
+        pass
 
-    Parameters
-    ----------
-    df : pd.DataFrame
-        元のデータ
+    def encode_onehot(self, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        one-hotエンコーディングを行う
 
-    Returns
-    -------
-    pd.DataFrame
-        one-hotエンコーディング後のデータ
-    """
-    assert isinstance(df, pd.DataFrame), "dfはpd.DataFrame型にしてください"
+        Parameters
+        ----------
+        df : pd.DataFrame
+            元のデータ
 
-    # nan以外の値を抽出する
-    item_list = df.values.flatten()
-    item_list = [x for x in item_list if pd.notnull(x)]
+        Returns
+        -------
+        pd.DataFrame
+            one-hotエンコーディング後のデータ
+        """
+        assert isinstance(df, pd.DataFrame), "dfはpd.DataFrame型にしてください"
 
-    # item_listから重複を除いたリストを作成する
-    item_list = list(set(item_list))
-    # item_listをabc順にソートする
-    item_list.sort()
+        # nan以外の値を抽出する
+        item_list = df.values.flatten()
+        item_list = [x for x in item_list if pd.notnull(x)]
 
-    # item_listをcolumns、indexはdfと同じ、値はFalseのDataFrameを作成する
-    one_hot_df = pd.DataFrame(False, columns=item_list, index=df.index)
+        # item_listから重複を除いたリストを作成する
+        item_list = list(set(item_list))
+        # item_listをabc順にソートする
+        item_list.sort()
 
-    # dfの各行でforループを回す
-    for row_index, row in df.iterrows():
-        # 中身のあるセルを抽出する
-        items = [x for x in row if pd.notnull(x)]
-        for item in items:
-            for column in one_hot_df.columns:
-                if item == column:
-                    one_hot_df.loc[row_index, column] = True
+        # item_listをcolumns、indexはdfと同じ、値はFalseのDataFrameを作成する
+        one_hot_df = pd.DataFrame(False, columns=item_list, index=df.index)
 
-    test_encode_onehot(df, one_hot_df)
+        # dfの各行でforループを回す
+        for row_index, row in df.iterrows():
+            # 中身のあるセルを抽出する
+            items = [x for x in row if pd.notnull(x)]
+            for item in items:
+                for column in one_hot_df.columns:
+                    if item == column:
+                        one_hot_df.loc[row_index, column] = True
 
-    return one_hot_df
+        self.test_encode_onehot(df, one_hot_df)
 
-
-def test_encode_onehot(df: pd.DataFrame, one_hot_df: pd.DataFrame) -> None:
-    """
-    encode_onehot関数のテストを行う
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        元のデータ
-
-    one_hot_df : pd.DataFrame
-        one-hotエンコーディング後のデータ
-    """
-    assert isinstance(df, pd.DataFrame), "dfはpd.DataFrame型にしてください"
-    assert isinstance(one_hot_df, pd.DataFrame), "one_hot_dfはpd.DataFrame型にしてください"
-    assert df.index.equals(one_hot_df.index), "dfとone_hot_dfのindexが異なります"
-
-    # dfの各行でforループを回す
-    for row_index, row in df.iterrows():
-        # 中身のあるセルを抽出する
-        a_items = [x for x in row if pd.notnull(x)]
-        # one_hot_dfの行を抽出する
-        b_items = one_hot_df.loc[row_index, :]
-        # Trueの値を抽出する
-        b_items = b_items[b_items == True]
-        # indexを抽出する
-        b_items = b_items.index.values
-        # a_itemsとb_itemsが一致するか確認する
-        assert set(a_items) == set(b_items), "one-hotエンコーディングが正しく行われていません"
+        return one_hot_df
 
 
-def load_onehot(file_path: str) -> pd.DataFrame:
-    """
-    one-hotエンコーディング後のデータを読み込む
+    def test_encode_onehot(self, df: pd.DataFrame, one_hot_df: pd.DataFrame) -> None:
+        """
+        encode_onehot関数のテストを行う
 
-    Parameters
-    ----------
-    file_path : str
-        ファイルパス
+        Parameters
+        ----------
+        df : pd.DataFrame
+            元のデータ
 
-    Returns
-    -------
-    pd.DataFrame
-        one-hotエンコーディング後のデータ
-    """
-    assert os.path.exists(file_path), "ファイルが存在しません"
-    assert isinstance(file_path, str), "file_pathはstr型にしてください"
-    assert file_path.endswith(".csv"), "CSVファイルを指定してください"
+        one_hot_df : pd.DataFrame
+            one-hotエンコーディング後のデータ
+        """
+        assert isinstance(df, pd.DataFrame), "dfはpd.DataFrame型にしてください"
+        assert isinstance(one_hot_df, pd.DataFrame), "one_hot_dfはpd.DataFrame型にしてください"
+        assert df.index.equals(one_hot_df.index), "dfとone_hot_dfのindexが異なります"
 
-    # onehotデータがある場合はそれを読み込む
-    if os.path.exists(f"{file_path}.onehot.csv"):
-        one_hot_df = pd.read_csv(f"{file_path}.onehot.csv", header=0)
-    else:
-        # CSVファイルからデータを読み込む
-        # 1行目にはヘッダがあるので、ヘッダを指定する
-        df = pd.read_csv(file_path, header=0)
-        # print(df)
-        # print(type(df))
+        # dfの各行でforループを回す
+        for row_index, row in df.iterrows():
+            # 中身のあるセルを抽出する
+            a_items = [x for x in row if pd.notnull(x)]
+            # one_hot_dfの行を抽出する
+            b_items = one_hot_df.loc[row_index, :]
+            # Trueの値を抽出する
+            b_items = b_items[b_items == True]
+            # indexを抽出する
+            b_items = b_items.index.values
+            # a_itemsとb_itemsが一致するか確認する
+            assert set(a_items) == set(b_items), "one-hotエンコーディングが正しく行われていません"
 
-        # one-hotエンコーディングを行う
-        one_hot_df = encode_onehot(df)
-        # print(one_hot_df)
 
-        # one-hotエンコーディング後のデータをCSVファイルに書き込む
-        one_hot_df.to_csv(f"{file_path}.onehot.csv", index=False)
+    def load_onehot(self, file_path: str) -> pd.DataFrame:
+        """
+        one-hotエンコーディング後のデータを読み込む
 
-    return one_hot_df
+        Parameters
+        ----------
+        file_path : str
+            ファイルパス
+
+        Returns
+        -------
+        pd.DataFrame
+            one-hotエンコーディング後のデータ
+        """
+        assert os.path.exists(file_path), "ファイルが存在しません"
+        assert isinstance(file_path, str), "file_pathはstr型にしてください"
+        assert file_path.endswith(".csv"), "CSVファイルを指定してください"
+
+        # onehotデータがある場合はそれを読み込む
+        if os.path.exists(f"{file_path}.onehot.csv"):
+            one_hot_df = pd.read_csv(f"{file_path}.onehot.csv", header=0)
+        else:
+            # CSVファイルからデータを読み込む
+            # 1行目にはヘッダがあるので、ヘッダを指定する
+            df = pd.read_csv(file_path, header=0)
+            # print(df)
+            # print(type(df))
+
+            # one-hotエンコーディングを行う
+            one_hot_df = self.encode_onehot(df)
+            # print(one_hot_df)
+
+            # one-hotエンコーディング後のデータをCSVファイルに書き込む
+            one_hot_df.to_csv(f"{file_path}.onehot.csv", index=False)
+
+        return one_hot_df
+
+
+class AssociationRuleMining:
+    def __init__(self) -> None:
+        pass
 
 
 if __name__ == "__main__":
     # コマンドライン引数からファイルパスを取得
     file_path = sys.argv[1]
 
-    one_hot_df = load_onehot(file_path)
+    one_hot_df = OneHotEncoder().load_onehot(file_path)
+    # print(one_hot_df)
 
-    print(one_hot_df)
+    # supportを計算する
