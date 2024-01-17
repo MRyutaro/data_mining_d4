f5e42c961013a7f7988aa654b46d3e19333594f1
ryutaro <m2002r1028@icloud.com>
2024-01-17 21:28:26 +0900

    fix: 引数

--- dbscan.py
+++ dbscan.py
@@ -5,12 +5,12 @@ import pandas as pd
 from sklearn.cluster import DBSCAN
 
 
-def calculate_dbscan(file_path, k):
+def calculate_dbscan(file_path, eps, min_samples):
     # CSVファイルからデータを読み込む
     df = pd.read_csv(file_path, header=None, names=["x", "y"])
 
     # DBSCANクラスの初期化
-    dbscan = DBSCAN(eps=0.1)
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
 
     # データを学習させクラスタリングを行う
     clusters = dbscan.fit_predict(df)
@@ -30,12 +30,17 @@ def calculate_dbscan(file_path, k):
     plt.xlabel('x')
     plt.ylabel('y')
 
+    plt.savefig(r"data\output\dbscan-py-crater.pdf", format="pdf")
+
     # グラフを表示
-    plt.show()
+    # plt.show()
 
 
 if __name__ == "__main__":
     # コマンドライン引数からファイルパスを取得
-    file_path = sys.argv[1]
+    # file_path = sys.argv[1]
+    file_path = r"data\input\crater.csv"
 
-    calculate_dbscan(file_path, 3)
+    eps = 0.5
+    min_samples = 5
+    calculate_dbscan(file_path)
